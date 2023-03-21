be58c967eeae759aea5552684a78de91540b6104
Carolina Correa <carolina.unila@gmail.com>
2023-03-20 22:50:28 -0300

    Aula 4

--- aula_04/src/aula_04/Exercicio_01.java
+++ aula_04/src/aula_04/Exercicio_01.java
@@ -11,11 +11,11 @@ public class Exercicio_01 {
 
 		int numero;
 		int vetorInteiros[] = { 10, 38, 87, 55, 65, 23, 78, 41, 2, 6 };
-	
+		boolean encontrado = false;
 		
 		System.out.println("Digite um número: ");
 		numero = leia.nextInt();
-		boolean encontrado = false;
+		
 		
 		for (int valor = 0; valor < vetorInteiros.length; valor++) {
 			if (vetorInteiros[valor] == numero) { 
