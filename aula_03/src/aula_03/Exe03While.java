5ab5d47962c788a9ff0ae14c19b1120f7f6e9d35
Carolina Correa <carolina.unila@gmail.com>
2023-03-25 21:01:34 -0300

    Aula 03

--- aula_03/src/aula_03/Exe03While.java
+++ aula_03/src/aula_03/Exe03While.java
@@ -9,19 +9,23 @@ public class Exe03While {
 
 		int maior50 = 0, menor21 = 0, idade = 0;
 
-		System.out.println("Digite sua idade: ");
-		idade = leia.nextInt();
+		while (idade >= 0) {
+			System.out.println("Digite sua idade: ");
+			idade = leia.nextInt();
 
-		if (idade < 0)
-			System.out.println("Idade Inválida");
+			if (idade < 0) {
+				System.out.println("Idade Inválida");
+			}
 
-		while (idade > 50)
-			System.out.println("Total de pessoas maiores que 50 anos: " + maior50);
+			if (idade < 21 && idade > 0) {
+				menor21++;
+				System.out.println("Total de pessoas menores que 21 anos: " + menor21);
+			}
+			if (idade >=51) {
+				maior50++;
+				System.out.println("Total de pessoas maiores que 50 anos: " + maior50);
+			}
 
-		if (idade < 21 && idade > 0)
-			menor21++;
-
-		System.out.println("Total de pessoas menores que 21 anos: " + menor21);
-
+		}
 	}
 }
